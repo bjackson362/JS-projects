7ac1b578581772e5fe7a32ba67b5f4895931ca13
bjackson362 <88946765+bjackson362@users.noreply.github.com>
2023-01-25 12:40:15 -0700

    Add files via upload

---
+++ old-control-flow.js
@@ -0,0 +1,29 @@
+let userName = 'bjackson362';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'Can i truly become a Develoepr?';
+
+console.log(`${userName}, asked - ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+if (randomNumber === 0) {
+  console.log('It is certain');
+} else if (randomNumber === 1) {
+  console.log('It is decidedly so');
+} else if (randomNumber === 2) {
+  console.log('Reply hazy try again');
+} else if (randomNumber === 3) {
+  console.log('Cannot predict now');
+} else if (randomNumber === 4) {
+  console.log('Do not count on it');
+} else if (randomNumber === 5) {
+  console.log('My sources say no');
+} else if (randomNumber === 6) {
+  console.log('Outlook not so good');
+} else if (randomNumber === 7) {
+  console.log('Signs point to yes');
+}
